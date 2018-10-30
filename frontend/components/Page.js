b62cebc334afd3f544268f692d3ebdb0eb3c060b
datdevboi <cami172013@gmail.com>
2018-10-29 21:39:51 -0500

    Created Page component that will bring in our Meta and Header components

---
+++ frontend/components/Page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header';
+import Meta from './Meta';
+export default class Page extends React.Component {
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header/>
+        {this.props.children}
+      </div>
+    )
+  }
+}
