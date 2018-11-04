1718336244f78db04b747af8d713e1cef78fecc8
datdevboi <cami172013@gmail.com>
2018-11-03 21:24:08 -0500

    created items query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const Query = {
+  items: async (parent, args, ctx, info) => {
+    const items = await ctx.db.query.items();
+
+    return items;
+  }
+};
 
 module.exports = Query;
