6d890b086953a1718f164c7236b76dce0e34cbf1
datdevboi <cami172013@gmail.com>
2018-11-03 21:07:35 -0500

    created our createItem resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const mutations = {};
+const mutations = {
+  createItem: async (parent, args, ctx, info) => {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
+  }
+};
 
 module.exports = mutations;
