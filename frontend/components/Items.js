98c4b0392e1b90d512345d1e4131db531cc4de75
datdevboi <cami172013@gmail.com>
2018-11-04 08:22:53 -0600

    Made a few styled components center and separate our items

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import styled from "styled-components";
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -14,20 +15,41 @@ const ALL_ITEMS_QUERY = gql`
     }
   }
 `;
+
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
 export default class Items extends Component {
   render() {
     return (
-      <div>
+      <Center>
+        <p>Items</p>
         <Query query={ALL_ITEMS_QUERY}>
           {({ loading, error, data }) => {
             if (loading) {
               return <p>Loading..</p>;
             }
-            return <p>Im the childof query</p>;
+            if (error) {
+              return <p>Error: ${error.message}</p>;
+            }
+            return (
+              <ItemsList>
+                {data.items.map((item, index) => (
+                  <p key={index}>{item.title}</p>
+                ))}
+              </ItemsList>
+            );
           }}
         </Query>
-        <p>Items</p>
-      </div>
+      </Center>
     );
   }
 }
