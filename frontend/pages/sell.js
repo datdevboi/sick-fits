fcd2a89de7f9255bcbecba88b766f63d226a8066
datdevboi <cami172013@gmail.com>
2018-11-04 17:34:18 -0600

    Imported our CreateItem in our sell page and returned that component

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,8 +1,9 @@
-const Sell =  (props) => (
-    <div>
-        Sell
-    </div>
-);
+import CreateItem from "../components/CreateItem";
 
+const Sell = props => (
+  <div>
+    <CreateItem />
+  </div>
+);
 
 export default Sell;
