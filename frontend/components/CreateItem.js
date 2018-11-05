587c8c6fb2f4cb7cedaa596807ed7eb0577175e9
datdevboi <cami172013@gmail.com>
2018-11-04 19:39:21 -0600

    Made an input for our iamage and made a function that will upload the image and receive the url where it is stored and send that to our database

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -42,6 +42,29 @@ export default class CreateItem extends Component {
       [name]: val
     });
   };
+
+  uploadFile = async e => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "sickfits");
+
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/datdevboi/image/upload",
+      {
+        method: "POST",
+        body: data
+      }
+    );
+
+    const file = await res.json();
+    console.log(file);
+
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -61,6 +84,21 @@ export default class CreateItem extends Component {
             >
               <Error error={error} />
               <fieldset disabled={loading} aria-busy={loading}>
+                <label htmlFor="file">
+                  Image
+                  <input
+                    type="file"
+                    id="file"
+                    name="file"
+                    placeholder="Upload an image"
+                    required
+                    onChange={this.uploadFile}
+                  />
+                  {this.state.image && (
+                    <img src={this.state.image} alt="Upload Preview" />
+                  )}
+                </label>
+
                 <label htmlFor="title">Title</label>
                 <input
                   type="text"
