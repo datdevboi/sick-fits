c4ad919d8e28fa832e9dd17231d7fdb72c7e9cb3
datdevboi <cami172013@gmail.com>
2018-11-04 18:44:30 -0600

    Made the qgl mutation to create item and used it in our Mutation component

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,24 +1,28 @@
 import React, { Component } from "react";
 import { Mutation } from "react-apollo";
+import Router from "next/router";
 import gql from "graphql-tag";
 import Form from "./styles/Form";
+import Error from "./ErrorMessage";
 
-export const CREATE_ITEM_MUTATION = `
-    mutation CREATE_ITEM_MUTATION($title: String!
-            $description: String!
-            $price: Int!
-            image: String
-            largeImage: String
-        ) {
-        createItem(title: $title
-                description: $description
-                price: $price
-                image: $image
-                largeImage: $largeImage
-            ) {
-                id
-            }
+export const CREATE_ITEM_MUTATION = gql`
+  mutation CREATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      price: $price
+      image: $image
+      largeImage: $largeImage
+    ) {
+      id
     }
+  }
 `;
 export default class CreateItem extends Component {
   state = {
@@ -40,47 +44,58 @@ export default class CreateItem extends Component {
   };
   render() {
     return (
-      <Form
-        onSubmit={e => {
-          e.preventDefault();
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+        {(createItem, { loading, error, called, data }) => {
+          return (
+            <Form
+              onSubmit={async e => {
+                e.preventDefault();
 
-          console.log(this.state);
+                const response = await createItem();
+                console.log(response);
+                Router.push({
+                  pathname: "/item",
+                  query: { id: response.data.createItem.id }
+                });
+              }}
+            >
+              <Error error={error} />
+              <fieldset disabled={loading} aria-busy={loading}>
+                <label htmlFor="title">Title</label>
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  required
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="price">Price</label>
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  required
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                />
+                <label htmlFor="price">Description</label>
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Enter A Description"
+                  required
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                />
+                <button type="submit">Submit</button>
+              </fieldset>
+            </Form>
+          );
         }}
-      >
-        <h2>Sell an Item</h2>
-        <fieldset>
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            placeholder="Title"
-            required
-            value={this.state.title}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="price">Price</label>
-          <input
-            type="number"
-            id="price"
-            name="price"
-            placeholder="Price"
-            required
-            value={this.state.price}
-            onChange={this.handleChange}
-          />
-          <label htmlFor="price">Description</label>
-          <textarea
-            id="description"
-            name="description"
-            placeholder="Enter A Description"
-            required
-            value={this.state.description}
-            onChange={this.handleChange}
-          />
-          <button type="submit">Submit</button>
-        </fieldset>
-      </Form>
+      </Mutation>
     );
   }
 }
