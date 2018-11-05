dc8ec3c74caa3f4bd36c5e41e8f9c17ac29efbe0
datdevboi <cami172013@gmail.com>
2018-11-05 17:29:04 -0600

    Implemented the item query by forwarding to the db using prisma-binding

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,9 +1,12 @@
+const { forwardTo } = require("prisma-binding");
+
 const Query = {
   items: async (parent, args, ctx, info) => {
     const items = await ctx.db.query.items();
 
     return items;
-  }
+  },
+  item: forwardTo("db")
 };
 
 module.exports = Query;
