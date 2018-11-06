a099165e9c6eafa54671181cba5eab96fd0a71cd
datdevboi <cami172013@gmail.com>
2018-11-06 16:21:30 -0600

    Updated our DeleteItem component created a graphql mutation and made use of Mutation component

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
 
+export const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`;
 export default class DeleteItem extends Component {
   render() {
-    return <button>{this.props.children}</button>;
+    return (
+      <Mutation
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+      >
+        {(deleteItem, { loading, error }) => {
+          return (
+            <button
+              onClick={() => {
+                if (confirm("Are you sure you want to delete this?")) {
+                  deleteItem();
+                }
+              }}
+            >
+              {this.props.children}
+            </button>
+          );
+        }}
+      </Mutation>
+    );
   }
 }
