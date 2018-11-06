4f501880a521278ab0ba46263e40ae9d1e27910f
datdevboi <cami172013@gmail.com>
2018-11-06 16:44:15 -0600

    Made an update function for our Mutation component, this will update the ui when an item is deleted

--- frontend/components/DeleteItem.js
+++ frontend/components/DeleteItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import { ALL_ITEMS_QUERY } from "./Items";
 
 export const DELETE_ITEM_MUTATION = gql`
   mutation DELETE_ITEM_MUTATION($id: ID!) {
@@ -10,11 +11,21 @@ export const DELETE_ITEM_MUTATION = gql`
   }
 `;
 export default class DeleteItem extends Component {
+  update = (cache, payload) => {
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+
+    data.items = data.items.filter(item => {
+      return item.id !== payload.data.deleteItem.id;
+    });
+
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  };
   render() {
     return (
       <Mutation
         mutation={DELETE_ITEM_MUTATION}
         variables={{ id: this.props.id }}
+        update={this.update}
       >
         {(deleteItem, { loading, error }) => {
           return (
