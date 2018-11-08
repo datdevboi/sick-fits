5aaea34976a4e2896cf5277823d3c073d03b7c8f
datdevboi <cami172013@gmail.com>
2018-11-08 13:40:09 -0600

    Imported our Signout component and rendered it to when the user is logged in

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
 import Error from "./ErrorMessage";
+import Signout from "./Signout";
 
 const Nav = () => (
   <User>
@@ -22,6 +23,7 @@ const Nav = () => (
             <Link href="/me">
               <a>Account</a>
             </Link>
+            <Signout />
           </>
         )}
 
