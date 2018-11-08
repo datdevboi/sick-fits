1d5687719f8b2aed0bedee6931dbc05ee30d5b13
datdevboi <cami172013@gmail.com>
2018-11-08 13:39:41 -0600

    Made our signout mutation resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -84,6 +84,13 @@ const mutations = {
     });
 
     return user;
+  },
+  signout: (parent, args, ctx, info) => {
+    ctx.response.clearCookie("token");
+
+    return {
+      message: "Goodbye"
+    };
   }
 };
 
