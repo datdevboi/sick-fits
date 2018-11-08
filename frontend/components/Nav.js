b34d21613bf3fd5b2849842a90b59909993b2b1e
datdevboi <cami172013@gmail.com>
2018-11-08 13:15:57 -0600

    Updated our Nav component conditionally render some nav items on where or not the user is logged in or not

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,31 +4,35 @@ import User from "./User";
 import Error from "./ErrorMessage";
 
 const Nav = () => (
-  <NavStyles>
-    <User>
-      {({ data, error, loading }) => {
-        if (loading) return <p>Loading...</p>;
-        if (error) return <Error error={error} />;
-        if (!data.me) return null;
-        return <p>{data.me.name}</p>;
-      }}
-    </User>
-    <Link href="/items">
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-  </NavStyles>
+  <User>
+    {({ data: { me }, error, loading }) => (
+      <NavStyles>
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+
+        {me && (
+          <>
+            <Link href="/sell">
+              <a>Sell</a>
+            </Link>
+            <Link href="/orders">
+              <a>Orders</a>
+            </Link>
+            <Link href="/me">
+              <a>Account</a>
+            </Link>
+          </>
+        )}
+
+        {!me && (
+          <Link href="/signup">
+            <a>Sign In</a>
+          </Link>
+        )}
+      </NavStyles>
+    )}
+  </User>
 );
 
 export default Nav;
