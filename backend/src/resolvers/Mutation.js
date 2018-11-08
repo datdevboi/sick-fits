35138b183f41543610a8787464bd856a25a66d6c
datdevboi <cami172013@gmail.com>
2018-11-07 19:17:12 -0600

    Created our signup mutation resolver function

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const mutations = {
   createItem: async (parent, args, ctx, info) => {
     // TODO: Check if they are logged in
@@ -32,6 +35,30 @@ const mutations = {
     const item = await ctx.db.query.item({ where }, `{id, title}`);
 
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  signup: async (parent, args, ctx, info) => {
+    // lowecase their email
+    args.email = args.email.toLowerCase();
+    // hash the password
+    const password = await bcrypt.hash(args.password, 10);
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] }
+        }
+      },
+      info
+    );
+
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+
+    return user;
   }
 };
 
