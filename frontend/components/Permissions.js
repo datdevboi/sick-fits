618d55be5320bde6d0832b62e24e9206912000f0
datdevboi <cami172013@gmail.com>
2018-11-10 09:51:00 -0600

    Created our UPDATE_PERMISSIONS_UPDATE query

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -1,4 +1,4 @@
-import { Query } from "react-apollo";
+import { Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
 import PropTypes from "prop-types";
 import Error from "../components/ErrorMessage";
@@ -56,6 +56,20 @@ const Permissions = props => (
     }}
   </Query>
 );
+
+const UPDATE_PERMISSIONS_MUTATION = gql`
+  mutation UPDATE_PERMISSIONS_MUTATION(
+    $userId: String
+    $permissions: [Permission]
+  ) {
+    updatePermissions(userId: $userId, permissions: $permissions) {
+      id
+      name
+      email
+      permissions
+    }
+  }
+`;
 
 class UserPermissions extends React.Component {
   static propTypes = {
