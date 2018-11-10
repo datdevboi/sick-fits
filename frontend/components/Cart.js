1b21503189ef52cfa0a57be628f5b83cdb4ae9c8
datdevboi <cami172013@gmail.com>
2018-11-10 13:44:53 -0600

    Added local start to our apollo client, we wrote two queries in our Cart.js file one to get the data from our apollo client and the second one to update data in our local state in apollo

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,22 +1,49 @@
 import React from "react";
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
 import CartStyles from "../components/styles/CartStyles";
 import Supreme from "../components/styles/CartStyles";
 import CloseButton from "../components/styles/CloseButton";
 import SickButton from "../components/styles/SickButton";
 
+export const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`;
+
+export const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`;
 const Cart = () => {
   return (
-    <CartStyles open>
-      <header>
-        <CloseButton title="close">&times;</CloseButton>
-        <Supreme>Your Cart</Supreme>
-        <p>You have __ Items in your cart.</p>
-      </header>
-      <footer>
-        <p>$10.10</p>
-        <SickButton>Checkout</SickButton>
-      </footer>
-    </CartStyles>
+    <Mutation mutation={TOGGLE_CART_MUTATION}>
+      {toggleCart => {
+        return (
+          <Query query={LOCAL_STATE_QUERY}>
+            {({ data, error, loading }) => {
+              return (
+                <CartStyles open={data.cartOpen}>
+                  <header>
+                    <CloseButton title="close" onClick={toggleCart}>
+                      &times;
+                    </CloseButton>
+                    <Supreme>Your Cart</Supreme>
+                    <p>You have __ Items in your cart.</p>
+                  </header>
+                  <footer>
+                    <p>$10.10</p>
+                    <SickButton>Checkout</SickButton>
+                  </footer>
+                </CartStyles>
+              );
+            }}
+          </Query>
+        );
+      }}
+    </Mutation>
   );
 };
 
