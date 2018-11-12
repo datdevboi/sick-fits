418ca0b7448c93f8b4fd787afb73217ea99cd033
datdevboi <cami172013@gmail.com>
2018-11-11 18:14:26 -0600

    Fixed an issue with our signin resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -87,19 +87,11 @@ const mutations = {
     return user;
   },
   signin: async (parent, { email, password }, ctx, info) => {
-    const user = await ctx.db.query.user(
-      {
-        where: {
-          email
-        }
-      },
-      `{
-        password
-        id
-        name
+    const user = await ctx.db.query.user({
+      where: {
         email
-      }`
-    );
+      }
+    });
 
     if (!user) {
       throw new Error("No user with that email exist");
