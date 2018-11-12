24929f667dd941618900a1049dd9a6462a415c1a
datdevboi <cami172013@gmail.com>
2018-11-11 18:16:09 -0600

    Made use of cart Items

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import User from "./User";
 import CartStyles from "../components/styles/CartStyles";
 import Supreme from "../components/styles/CartStyles";
 import CloseButton from "../components/styles/CloseButton";
 import SickButton from "../components/styles/SickButton";
+import CartItem from "./CartItem";
+import calcTotalPrice from "../lib/calcTotalPrice";
+import formatMoney from "../lib/formatMoney";
 
 export const LOCAL_STATE_QUERY = gql`
   query {
@@ -19,31 +23,50 @@ export const TOGGLE_CART_MUTATION = gql`
 `;
 const Cart = () => {
   return (
-    <Mutation mutation={TOGGLE_CART_MUTATION}>
-      {toggleCart => {
+    <User>
+      {({ data: { me } }) => {
+        if (!me) return null;
+
         return (
-          <Query query={LOCAL_STATE_QUERY}>
-            {({ data, error, loading }) => {
+          <Mutation mutation={TOGGLE_CART_MUTATION}>
+            {toggleCart => {
               return (
-                <CartStyles open={data.cartOpen}>
-                  <header>
-                    <CloseButton title="close" onClick={toggleCart}>
-                      &times;
-                    </CloseButton>
-                    <Supreme>Your Cart</Supreme>
-                    <p>You have __ Items in your cart.</p>
-                  </header>
-                  <footer>
-                    <p>$10.10</p>
-                    <SickButton>Checkout</SickButton>
-                  </footer>
-                </CartStyles>
+                <Query query={LOCAL_STATE_QUERY}>
+                  {({ data, error, loading }) => {
+                    return (
+                      <CartStyles open={data.cartOpen}>
+                        <header>
+                          <CloseButton title="close" onClick={toggleCart}>
+                            &times;
+                          </CloseButton>
+                          <Supreme>
+                            {me.name}
+                            's Cart
+                          </Supreme>
+                          <p>
+                            You have {me.cart.length} Item
+                            {me.cart.length === 1 ? "" : "s"} in your cart.
+                          </p>
+                        </header>
+                        <ul>
+                          {me.cart.map(cartItem => {
+                            return <CartItem cartItem={cartItem} />;
+                          })}
+                        </ul>
+                        <footer>
+                          <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+                          <SickButton>Checkout</SickButton>
+                        </footer>
+                      </CartStyles>
+                    );
+                  }}
+                </Query>
               );
             }}
-          </Query>
+          </Mutation>
         );
       }}
-    </Mutation>
+    </User>
   );
 };
 
