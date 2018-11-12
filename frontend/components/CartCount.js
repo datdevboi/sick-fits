0595cfc232e74f1030912f5efaaf019f833ea960
datdevboi <cami172013@gmail.com>
2018-11-11 22:25:03 -0600

    Added new animation to our CartCount each time we add something to the cart

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -1,7 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
+const AnimationStyles = styled.span`
+  position: relative;
+  .count {
+    display: block;
+    position: relative;
+    transition: all 0.4s;
+    backface-visibility: hidden;
+  }
+
+  .count-enter {
+    transform: scale(4) rotateX(0.5turn);
+  }
+
+  .count-enter-active {
+    transform: rotateX(0);
+  }
+
+  .count-exit {
+    top: 0;
+    position: absolute;
+    transform: rotateX(0);
+  }
+
+  .count-exit-active {
+    transform: rotateX(0.5turn);
+  }
+`;
 const Dot = styled.div`
   background: ${props => props.theme.red};
   color: white;
@@ -16,7 +44,21 @@ const Dot = styled.div`
 `;
 
 function CartCount({ count }) {
-  return <Dot>{count}</Dot>;
+  return (
+    <AnimationStyles>
+      <TransitionGroup>
+        <CSSTransition
+          unmountOnExit
+          className="count"
+          classNames="count"
+          key={count}
+          timeout={{ enter: 400, exit: 400 }}
+        >
+          <Dot>{count}</Dot>
+        </CSSTransition>
+      </TransitionGroup>
+    </AnimationStyles>
+  );
 }
 
 CartCount.propTypes = {};
