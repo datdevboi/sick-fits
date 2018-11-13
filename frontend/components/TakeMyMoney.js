71bc1d683f51d9419e09be27ea8bd87fa961e5dc
datdevboi <cami172013@gmail.com>
2018-11-12 20:10:01 -0600

    Made createOrder mutation and used Mutation component to call the mutation

--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -9,6 +9,20 @@ import calcTotalPrice from "../lib/calcTotalPrice";
 import Error from "./ErrorMessage";
 import User, { CURRENT_USER_QUERY } from "./User";
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation CREATE_ORDER_MUTATION($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`;
+
 function totalItems(cart) {
   return cart.reduce((tally, cartItem) => {
     return tally + cartItem.quantity;
@@ -16,24 +30,39 @@ function totalItems(cart) {
 }
 
 class TakeMyMoney extends React.Component {
-  onToken = res => {};
+  onToken = (res, createOrder) => {
+    createOrder({
+      variables: {
+        token: res.id
+      }
+    }).catch(e => alert(e.message));
+  };
   render() {
     return (
       <User>
         {({ data: { me } }) => {
           return (
-            <StripeCheckout
-              amount={calcTotalPrice(me.cart)}
-              description={`Order of ${totalItems(me.cart)} items!`}
-              name="Sick Fits"
-              image={me.cart[0].item && me.cart[0].item.image}
-              stripeKey={"pk_test_3BsLpmZIJHmzAG4PweXNZjtm"}
-              currency="USD"
-              email={me.email}
-              token={res => this.onToken(res)}
+            <Mutation
+              mutation={CREATE_ORDER_MUTATION}
+              refetchQueries={[{ query: CURRENT_USER_QUERY }]}
             >
-              {this.props.children}
-            </StripeCheckout>
+              {createOrder => {
+                return (
+                  <StripeCheckout
+                    amount={calcTotalPrice(me.cart)}
+                    description={`Order of ${totalItems(me.cart)} items!`}
+                    name="Sick Fits"
+                    image={me.cart[0].item && me.cart[0].item.image}
+                    stripeKey={"pk_test_3BsLpmZIJHmzAG4PweXNZjtm"}
+                    currency="USD"
+                    email={me.email}
+                    token={res => this.onToken(res, createOrder)}
+                  >
+                    {this.props.children}
+                  </StripeCheckout>
+                );
+              }}
+            </Mutation>
           );
         }}
       </User>
